ac79cf855de10fd4c9af916032b08f99214a3987
Divaka Perera <thimiradivaka@gmail.com>
2023-08-05 23:24:26 +0530

    in progress...

--- pre_processing/preprocessor.py
+++ pre_processing/preprocessor.py
@@ -19,16 +19,22 @@ while cap.isOpened():
     if results.pose_landmarks:
         mp.solutions.drawing_utils.draw_landmarks(frame, results.pose_landmarks, mp_pose.POSE_CONNECTIONS)
     
-    print(results.pose_landmarks)
+    # print(results.pose_landmarks)
+    with open("break_1.csv", mode='a', newline='') as file:
+        j = results.pose_landmarks.landmark[0]
+        res = str(j.x) +','+ str(j.y)
+        file.write(res)
+        for i in range(1,33):
+            j = results.pose_landmarks.landmark[i]
+            print(j.x , j.y)
+            res = ',' + str(j.x) +','+ str(j.y)
+            file.write(res)
+        file.write("\n")
 
     cv2.imshow('skeleton', frame)
-
-    cv2.waitKey(0)
     
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-
-    break
     
 cap.release()
 cv2.destroyAllWindows()
