41da07369cedf76f1687aad397b7341a3d3ddcf7
Divaka Perera <thimiradivaka@gmail.com>
2023-08-06 11:45:55 +0530

    done

--- pre_processing/preprocessor.py
+++ pre_processing/preprocessor.py
@@ -1,11 +1,12 @@
 import mediapipe as mp
 import cv2
+from mediapipe.framework.formats import landmark_pb2
 
 mp_pose = mp.solutions.pose
 pose = mp_pose.Pose()
 
 cap = cv2.VideoCapture("break_1.avi")
-
+start = 1
 while cap.isOpened():
     ret, frame = cap.read()
 
@@ -16,25 +17,37 @@ while cap.isOpened():
 
     results = pose.process(rgbFrame)
 
+    finalRes = landmark_pb2.NormalizedLandmarkList(landmark = results.pose_landmarks.landmark[11:23])
+
+    connections = frozenset({(4, 10), (5, 9), (7, 9), (3, 5),  (4, 6), (5, 11), (0, 2), (4, 8), (5, 7), (1, 3), (6, 8),  (2, 4)})
+
     if results.pose_landmarks:
-        mp.solutions.drawing_utils.draw_landmarks(frame, results.pose_landmarks, mp_pose.POSE_CONNECTIONS)
+        mp.solutions.drawing_utils.draw_landmarks(frame, finalRes, connections)
     
-    # print(results.pose_landmarks)
+    # for i in finalRes.landmark:
+    #     print(i.x, i.y)
     with open("break_1.csv", mode='a', newline='') as file:
-        j = results.pose_landmarks.landmark[0]
+        if start==0:
+            file.write("\n")
+            
+        j = finalRes.landmark[0]
         res = str(j.x) +','+ str(j.y)
+        print(j.x , j.y)
         file.write(res)
-        for i in range(1,33):
-            j = results.pose_landmarks.landmark[i]
+
+        for i in range(1,12):
+            j = finalRes.landmark[i]
             print(j.x , j.y)
             res = ',' + str(j.x) +','+ str(j.y)
             file.write(res)
-        file.write("\n")
+        
+        start = 0
+        
 
     cv2.imshow('skeleton', frame)
     
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-    
+
 cap.release()
 cv2.destroyAllWindows()
